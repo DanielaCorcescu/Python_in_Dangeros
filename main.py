1c27380672abef8685d05a2b6f8abda457afdc7f
Corcescu <corcescudaniela@yahoo.com>
2021-06-15 17:59:12 +0200

    initial comit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+import winsound
+import Player
+
+
 introMsg = """ 
 *****************************************************************************************
 * Welcome,stranger.                                                                     *
@@ -8,6 +12,8 @@ introMsg = """
 *****************************************************************************************
 """
 
+sound = "Main_Menu.wav"
+winsound.PlaySound(sound, winsound.SND_ASYNC)
 print(introMsg)
 print("Would you like to start the adventure ?")
 print("Yes or No")
@@ -15,6 +21,34 @@ user_answer = input("Yes or No -> ")
 if user_answer.upper() == "YES":
     print("OK")
     os.system("cls") #windows
+    answer = input("""What type of player are you: 
+1 for Warrior 2 for Wizard -> """)
+    if answer == '1':
+        player_name = input("""You heve chosen to be a mighty warrior 
+What is your name? -> """)
+        player = Player.Warrior(player_name)
+        print("Intro the world")
+        input("Press a key to continue... ")
 
+        os.system('cls')
+        sound = "Exploring.wav"
+        winsound.PlaySound(sound, winsound.SND_ASYNC)
+        print("""You are in the middle of a crossroads.
+You have 3 paths in front of you:
+1. Going to a village
+2. Going to a forest
+3. Going to desert  """)
+        path_option = input("What is your destiny? ")
+
+
+    elif answer == '2':
+        player_name = input("""You heve chosen to be a mighty wizard 
+What is your name? -> """)
+        player = Player.Wizard(player_name)
+        print("Intro the world")
+        os.system('cls')
+
+    else:
+        print("Chose a valid option")
 else:
     print("Thanck you, good bye")
